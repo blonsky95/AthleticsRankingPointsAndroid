09806cb203df958a939f2a5a7ac8075670d44aab
blonsky95 <pablo.trescoli@yahoo.es>
2022-06-12 16:54:29 +0200

    fixed divider bug in dropdown

--- app/src/main/java/com/tatoeapps/athleticsrankingpoints/presentation/components/DropdownItem.kt
+++ app/src/main/java/com/tatoeapps/athleticsrankingpoints/presentation/components/DropdownItem.kt
@@ -1,20 +1,29 @@
 package com.tatoeapps.athleticsrankingpoints.presentation.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.tatoeapps.athleticsrankingpoints.presentation.theme.AthleticsRankingPointsTheme
 import com.tatoeapps.athleticsrankingpoints.presentation.theme.beige
 
 @Composable
 fun DropdownItem(text: String, textColor: Color = AthleticsRankingPointsTheme.colors.backgroundComponent) {
-  Text(
-    text = text,
-    style = AthleticsRankingPointsTheme.typography.text4,
-    color = textColor
-  )
-  Divider(
-    color = beige,
-  )
+  Column {
+    Text(
+      text = text,
+      style = AthleticsRankingPointsTheme.typography.text4,
+      color = textColor
+    )
+    Spacer(modifier = Modifier.height(2.dp))
+    Divider(
+      color = beige,
+    )
+  }
+
 }
